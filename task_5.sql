8799bb67dbd1dcc19bdfbcda07f80f12be48ca2b
fiker robel <fikerrobel889@gmail.com>
2025-10-19 21:03:34 +0300

    Add task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Script: task_5.sql
+-- This script inserts a single row into the CUSTOMER table
+-- in the database alx_book_store
+
+INSERT INTO customer(customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
