aeb3ca111a6510c07af7966b804c592e543bfef5
fiker robel <fikerrobel889@gmail.com>
2025-10-19 20:56:27 +0300

    Fix task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,4 +1,4 @@
--- Use the database
+-- -- Use the database
 USE alx_book_store;
 
 -- Table: Authors
@@ -33,12 +33,4 @@ CREATE TABLE IF NOT EXISTS orders (
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
--- Table: Order_Details
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
+-- Table: Order
